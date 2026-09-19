f377583d3e106c9375fae66df51524500109e473
Mahboob Alam <connect.mahboobalam@gmail.com>
2026-09-19 19:14:53 +0530

    fix(routing): serve static files in debug and configure root manifest and favicon shortcuts

--- fuel_router/urls.py
+++ fuel_router/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path, re_path
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
@@ -32,7 +34,36 @@ urlpatterns = [
         ),
         name="apple-touch-icon",
     ),
+    path(
+        "apple-touch-icon-precomposed.png",
+        RedirectView.as_view(
+            url="/static/favicons/apple-touch-icon.png", permanent=True
+        ),
+        name="apple-touch-icon-precomposed",
+    ),
+    path(
+        "site.webmanifest",
+        RedirectView.as_view(
+            url="/static/favicons/site.webmanifest", permanent=True
+        ),
+        name="site-webmanifest",
+    ),
+    path(
+        "manifest.json",
+        RedirectView.as_view(
+            url="/static/favicons/site.webmanifest", permanent=True
+        ),
+        name="manifest-json",
+    ),
+    re_path(
+        r"^(?P<icon>favicon-\d+x\d+\.png)$",
+        RedirectView.as_view(url="/static/favicons/%(icon)s", permanent=True),
+        name="favicon-resolution-icon",
+    ),
     path("api/", include("api.urls")),
     path("", include("ui.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
 
